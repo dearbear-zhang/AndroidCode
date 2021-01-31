8cec7238753d8bc2846ffee996f9d744e7082e7b
dearbear-zhang <dearbear_zhang@icloud.com>
2021-01-31 23:36:54 +0800

    [1]加入BRVAH;

--- app/src/main/java/com/mine/dearbear/view/MainActivity.java
+++ app/src/main/java/com/mine/dearbear/view/MainActivity.java
@@ -1,44 +1,78 @@
 package com.mine.dearbear.view;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.Button;
 import android.widget.Toast;
 
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.viewholder.BaseViewHolder;
 import com.dearbear.utilcode.socket.SocketServiceHelper;
-import com.example.toolkit.ui.slide.ScrollingTestActivity;
 import com.mine.dearbear.R;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends Activity {
     private String TAG = MainActivity.class.getName();
     private Button mTextBtn;
     private Handler mHandler = new Handler();
+    private ItemAdapter mItemAdapter;
+    private RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
-//        initData();
+        initData();
     }
 
     private void initView() {
-        mTextBtn = findViewById(R.id.test_btn);
-        mTextBtn.setOnClickListener(v -> {
-            SocketServiceHelper.getInstance().register(s -> {
-                mHandler.post(() -> {
-                    Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
-                });
+        mRecyclerView = findViewById(R.id.mainRv);
+    }
+
+    private void initData() {
+        List<String> data = new ArrayList<>();
+        data.add("基本信息");
+        data.add("应用信息");
+        data.add("图片,音频");
+        mItemAdapter = new ItemAdapter(data);
+        mRecyclerView.setAdapter(mItemAdapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+//        mItemAdapter.notifyDataSetChanged();
+
+//        Toast.makeText(this, "集成模式启动", Toast.LENGTH_LONG).show();
+//        Intent intent = new Intent(this, ScrollingTestActivity.class);
+//        startActivity(intent);
+    }
+
+    private void test() {
+        SocketServiceHelper.getInstance().register(s -> {
+            mHandler.post(() -> {
+                Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
             });
         });
     }
 
-    private void initData() {
-        Toast.makeText(this, "集成模式启动", Toast.LENGTH_LONG).show();
-        Intent intent = new Intent(this, ScrollingTestActivity.class);
-        startActivity(intent);
+
+    static class ItemAdapter extends BaseQuickAdapter<String, BaseViewHolder> {
+
+        public ItemAdapter(List<String> data) {
+            super(R.layout.common_item_title_click, data);
+        }
+
+        @Override
+        protected void convert(@NotNull BaseViewHolder baseViewHolder, String s) {
+            baseViewHolder.setText(R.id.commonItemTitleTv, s);
+        }
     }
+
 }
